09e8b1ebbe26627015f1d8eba73d608b0fa2feb1
jphc98 <jphc1998@hotmail.com>
2025-07-10 19:44:59 -0500

    Modificación de vista endpoint
    Se realizan cambios en el endpoint de los productos más vendidos por sucursal.

--- company/urls.py
+++ company/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("v1/", include(Franquicia_router.urls)),
     path("v1/", include(Sucursal_router.urls)),
     path("v1/", include(Producto_router.urls)),
-    path("v1/Prodxs", ProductosSucursal.as_view()),
+    path("v1/popularprods", ProductosSucursal.as_view()),
 
 ]
